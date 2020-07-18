d2cd86626dec6d3e53906ca4dd11455295270fdd
arainay <arainay@gmail.com>
2020-07-18 10:30:44 +0800

    Init

---
+++ ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memories
+//
+//  Created by Сергей Лобанов on 18.07.2020.
+//  Copyright © 2020 arainay. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("👻")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
